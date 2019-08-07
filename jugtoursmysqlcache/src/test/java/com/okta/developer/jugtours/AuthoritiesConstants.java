2892aef2437f29352dde33eb286ae5934cfe2ba6
susanag <smilagross@gmail.com>
2019-08-06 23:30:55 -0500

    actualizando archivos

---
+++ jugtoursmysqlcache/src/test/java/com/okta/developer/jugtours/AuthoritiesConstants.java
@@ -0,0 +1,13 @@
+package com.okta.developer.jugtours;
+
+public class AuthoritiesConstants {
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    private AuthoritiesConstants() {
+    }
+
+}
